6d14f2dbed828613b3dcf6dfa4f58ecf0d964213
Andreea Loredana Pogar <andreealoredana.pogar@pegaso2000.it>
2023-06-27 11:50:17 +0200

    Piatto e Cliente controller con findAll e save

--- src/main/java/it/pegaso2000/FastFoodApi/controller/ClienteController.java
+++ src/main/java/it/pegaso2000/FastFoodApi/controller/ClienteController.java
@@ -4,15 +4,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.pegaso2000.FastFoodApi.dto.ClienteDto;
 import it.pegaso2000.FastFoodApi.model.Cliente;
 import it.pegaso2000.FastFoodApi.service.ClienteSevice;
 
-@Controller
+@RestController("api/cliente")
 public class ClienteController {
 
 	private ClienteSevice service;
@@ -24,17 +28,31 @@ public class ClienteController {
 	}
 	
 	//metodi
-	@GetMapping("home")
-	public String list(Model model) {
-		List<Cliente> lista=service.findAll();
-		List<ClienteDto> risultato= lista.stream().map(cliente ->{
+	@GetMapping("all")
+	public ResponseEntity<List<ClienteDto>>all(){
+		List<Cliente> list =service.findAll();
+		List<ClienteDto>result= list.stream().map(cliente ->{
 			return entityToDto(cliente);
 		}).collect(Collectors.toList());
-			
-		model.addAttribute("lista",risultato);
-	
-		return "homepage";
+		return new ResponseEntity<List<ClienteDto>>(result,HttpStatus.OK);
 	}
+	
+	@GetMapping("getById/{id}")
+	public ResponseEntity<ClienteDto> getById(@PathVariable int id){
+		Cliente c = service.findById(id);
+		
+		if(c==null) return new ResponseEntity<ClienteDto>(HttpStatus.NO_CONTENT);
+		ClienteDto dto= entityToDto(c);
+		return new ResponseEntity<ClienteDto>(dto,HttpStatus.OK);
+	}
+
+	@PostMapping("/save")
+	public ResponseEntity<ClienteDto>save(@RequestBody ClienteDto dto){
+		Cliente saving =dtoToEntity(dto);
+		Cliente saved =service.save(saving);
+		return new ResponseEntity<ClienteDto>(entityToDto(saved),HttpStatus.OK);
+	}
+	
 	
 	
 	private ClienteDto entityToDto(Cliente c) {
@@ -46,5 +64,13 @@ public class ClienteController {
 		return dto;
 	}
 	
+	private Cliente dtoToEntity (ClienteDto dto) {
+		Cliente c= new Cliente();
 	
+		c.setNome(dto.getNome());
+		c.setEta(dto.getEta());
+		c.setIs_vegetariano(dto.isIs_vegetariano());
+		return c;
+		
+	}
 }
--- src/main/java/it/pegaso2000/FastFoodApi/controller/PiattoController.java
+++ src/main/java/it/pegaso2000/FastFoodApi/controller/PiattoController.java
@@ -1,13 +1,21 @@
 package it.pegaso2000.FastFoodApi.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.pegaso2000.FastFoodApi.dto.PiattoDto;
 import it.pegaso2000.FastFoodApi.model.Piatto;
 import it.pegaso2000.FastFoodApi.service.PiattoService;
 
-@Controller
+@RestController("api/piatto")
 public class PiattoController {
 	private PiattoService service;
 
@@ -18,6 +26,29 @@ public class PiattoController {
 		
 	}
 	
+	//metodi
+	@GetMapping("all")
+	public ResponseEntity<List<PiattoDto>> all(){
+		List<Piatto> list =service.findAll();
+		List<PiattoDto> result = list.stream().map(piatto ->{
+			return entityToDto(piatto);
+		}).collect(Collectors.toList());
+		return new ResponseEntity<List<PiattoDto>>(result, HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("getById/{id}")
+	public ResponseEntity<PiattoDto>getById(@PathVariable int id){
+		Piatto p= service.findById(id);
+		
+		if(p==null) return new ResponseEntity<PiattoDto>(HttpStatus.OK);
+		PiattoDto dto =entityToDto(p);
+		return new ResponseEntity<PiattoDto>(dto, HttpStatus.OK);
+			
+	}
+	
+	
+	
 	private PiattoDto entityToDto (Piatto p) {
 		PiattoDto dto=new PiattoDto();
 		dto.setDescrizione(p.getDescrizione());
@@ -26,5 +57,7 @@ public class PiattoController {
 		dto.setIs_vegetariano(p.isIs_vegetariano());
 		return dto;
 	}
+	
+	
 
 }
